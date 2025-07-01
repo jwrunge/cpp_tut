87780d248ec93a462cdb0fdd786937f2dc935546
jwr <jwrunge@gmail.com>
2025-07-01 00:01:58 -0500

    Playing with types

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <cstdint> 
 
 int main() {
-    int w1 { 4 };
-    int w2 { 5 };
-    int w3 { 6 };
+    std::int_fast8_t w1 { 4 };
+    std::int_fast8_t w2 { sizeof(int) };
+    std::int_fast8_t w3 {};
+    std::cout << "Enter the weight of the third item: ";
+    std::cin >> w3;
     std::cout << "The sum of the weights is: " << w1 + w2 + w3 << std::endl;
     return 0;
 }
